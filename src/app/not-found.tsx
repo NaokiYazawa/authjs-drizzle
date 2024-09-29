80f5b478baefca67b7f7cc159afcc96de57f8dea
naoki-yazawa <naoki.yazawa@dena.com>
2024-09-30 07:50:24 +0900

    feat: modify tailwind css class

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="flex h-dvh flex-col items-center justify-center p-4 text-center">
-      <h1 className="mb-2 text-6xl font-bold">404</h1>
+    <div className="h-dvh flex flex-col items-center justify-center p-4 text-center">
+      <h1 className="mb-2 text-8xl font-bold">404</h1>
       <p className="mb-4 text-2xl font-semibold">ページが見つかりません。</p>
       <p className="mb-8 text-lg">
         このページはすでに削除されているか、URL が間違っている可能性があります。
